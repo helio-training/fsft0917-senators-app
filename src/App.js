48a1966213468c37c75f02c0051897861a745590
Thor Anderson <ta.anderson@gmail.com>
2017-10-07 09:48:38 -0600

    Removed references to Senators class

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Senators from './Senators'
-
 class App extends Component {
   render() {
     return (
@@ -12,9 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <Senators />
-        </p>
       </div>
     );
   }
