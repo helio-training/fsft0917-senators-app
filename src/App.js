6ae1201ff8a29670ceb97e46a3d4f2ff38ad7fcd
Thor Anderson <ta.anderson@gmail.com>
2017-10-13 16:30:10 -0600

    Working version before added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Senators from './components/Senators';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider>
+        <div className="App">
           <Senators />
-      </div>
+        </div>
+      </MuiThemeProvider>  
     );
   }
 }
--- src/components/Senator.js
+++ src/components/Senator.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import { Card, CardHeader, CardMedia, CardTitle } from 'material-ui/Card';
 
 export default class Senator extends Component {
 
     render() {
 
         const senator = this.props.senator;
+        const senatorName = `${senator.person.firstname} ${senator.person.lastname}`;
+
+        const picID = senator.person.link.slice(senator.person.link.lastIndexOf('/'));
+        // https://www.govtrack.us/data/photos/400013-200px.jpeg
+        // https://www.govtrack.us/congress/members/tammy_baldwin/400013
 
         return (
             <div className="senator">
-                <h4>{senator.person.firstname} {senator.person.lastname}</h4>
+{/*                 <h4>{senator.person.firstname} {senator.person.lastname}</h4>
                 <div>{senator.party}</div>
-                <div>{senator.startdate}</div>
+                <div>{senator.startdate}</div> */}
+                <Card>
+                    <CardHeader
+                        title={senatorName}
+                        subtitle={senator.phone}
+                        avatar={`https://www.govtrack.us/data/photos${picID}-50px.jpeg`}
+                    />
+                    <CardMedia overlay={<CardTitle title={senator.description} />} >
+                        <img src={`https://www.govtrack.us/data/photos${picID}-200px.jpeg`}
+                            alt={senatorName}/>    
+                    </CardMedia>
+                </Card>
             </div>
         )
     }
